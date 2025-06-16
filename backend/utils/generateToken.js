751a6bb171d1354bb721fd4476c3311eacfdb5d3
Abdul <abdmankhan@gmail.com>
2025-06-17 02:07:32 +0530

    17062025 Prod-v3

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    // secure: process.env.NODE_ENV !== "development",
+    secure : true,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     sameSite: "None",
     // this is important for cross-site cookies, when i had it as 'None' then i could use both the frontend and backend on localhost,
